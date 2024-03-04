5ddd447eef9a1e13890df697f5f0d65c3d5ea582
= <=>
2024-03-04 22:36:43 +0800

    bubble

---
+++ src/BubbleSort.java
@@ -0,0 +1,19 @@
+
+public class BubbleSort {
+     public void bubbleSort(int [] array){
+     for(int i=0; i<array.length-1;i++){
+    for(int j=0;j<array.length-i-1;j++){
+    if(array[j]>array[j+1]){
+    System.out.println("i value is" + i + "and j value is" + j);
+     int temporary = array[j];
+     array[j] = array[j+1];
+    array[j+1] = temporary;
+     }
+    }
+     }
+     for(int j=0;j<array.length;j++){
+     System.out.println(array[j]+",");
+     }
+    }
+    }
+    
